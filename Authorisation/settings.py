b932dbf50f052a78a49420770453201818c57199
Kkakashiii513 <kkakashi513hatakeeee@gmail.com>
2025-07-31 18:43:47 +0500

    commit

--- Authorisation/settings.py
+++ Authorisation/settings.py
@@ -40,7 +40,7 @@ env = environ.Env(DEBUG=(bool, False))
 
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
+ALLOWED_HOSTS = ["authorisation-3t0g.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
